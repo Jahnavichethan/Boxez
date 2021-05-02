6402604469f182f7e282292e091ede03ba9c4816
Jahnavi Chethan <76475419+Jahnavichethan@users.noreply.github.com>
2021-05-02 16:37:36 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,27 @@
+class Player{
+    constructor(x,y){
+        var options={
+            'restitution':1,
+            'frictionAir':0,
+            'density':0.8
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=40;
+        this.height=40;
+
+        World.add(world,this.body);
+    }
+     display(){
+     var pos = this.body.position;
+     var angle = this.body.angle;
+     push()
+     translate(pos.x,pos.y);
+     rotate(angle);
+     rectMode(CENTER);
+     strokeWeight(1);
+     stroke("black");
+     fill("#F79CC0");
+     rect(0, 0, this.width, this.height);
+     pop();
+     }
+}
